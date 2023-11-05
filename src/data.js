7fa5ff3c818eece3f3a5ec6b719b803ac856a025
Pranav Jindal <123830703+pranavjindal29@users.noreply.github.com>
2023-11-05 12:22:22 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,36 @@
+export const projects = [
+    {
+      title: "Clip Downloader",
+      subtitle: "Shell Scripting",
+      description:
+        "Simple Script which downloads only the specified part of a video",
+      image: "./project-1.gif",
+      link: "#",
+    },
+    {
+      title: "Movies and Songs Recommendation Website",
+      subtitle: "Django",
+      description:
+        "Website which recommends Songs and Movies",
+      image: "./project-2.gif",
+      link: "#",
+    },
+    {
+      title: "AIO Help App",
+      subtitle: "Kotlin and Jetpack Compose",
+      description:
+        "App to hire Carpenters, Plumbers, Gardeners, etc",
+      image: "./project-3.gif",
+      link: "#",
+    },
+    {
+      title: "Snack Squad",
+      subtitle: "Kotlin and Jetpack Compose",
+      description:
+        "App to get food delivery",
+      image: "./project-4.gif",
+      link: "#",
+    },
+  ];
+
+  export const skills = ["Java", "Kotlin", "HTML/CSS", "MERN Stack"];
